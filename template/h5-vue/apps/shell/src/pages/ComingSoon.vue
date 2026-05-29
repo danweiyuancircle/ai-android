baf22bd31fd10bd4538a54448b1a4a9a9d865ac2
liuyongjie <liuyj@chances.com.cn>
2026-05-29 10:13:23 +0800

    refactor(shell): ComingSoon 改用 EPage

--- template/h5-vue/apps/shell/src/pages/ComingSoon.vue
+++ template/h5-vue/apps/shell/src/pages/ComingSoon.vue
@@ -1,19 +1,15 @@
 <template>
-  <FocusSection id="coming-soon" :enter-to="'last-focused'" class="page coming-soon">
+  <EPage id="coming-soon" default-focus="soon-back" class="page coming-soon">
     <h1 class="title">敬请期待</h1>
     <div class="actions">
       <EButton focus-key="soon-back" label="返回" @enter="goBack" />
     </div>
-  </FocusSection>
+  </EPage>
 </template>
 
 <script setup lang="ts">
 import { useRouter } from 'vue-router'
-import { FocusSection } from '@dwy/focus-vue3'
-import { EButton } from '@dwy/tv-ui'
-import { useFocusPage } from '@shell/core'
-
-useFocusPage('coming-soon', 'soon-back')
+import { EPage, EButton } from '@dwy/tv-ui'
 
 const router = useRouter()
 const goBack = () => router.back()
